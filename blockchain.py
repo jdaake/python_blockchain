d3fb5f013650288905f0c77b25c90279e6890f9a
*J Daake* <jordandaake@JDaake-MacBook-Pro.local>
2020-08-07 13:50:52 -0500

    Adds else if and break statements.

--- blockchain.py
+++ blockchain.py
@@ -13,9 +13,11 @@ def get_transaction_value():
     """Gets the user input for the transaction amount. """
     return float(input('Enter the transaction amount: '))
 
+
 def get_user_choice():
     user_input = input('Your choice: ')
     return user_input
+
 
 def print_blockchain_element():
     for block in blockchain:
@@ -30,13 +32,17 @@ while True:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Print blockchain')
+    print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
         tx_amount = get_transaction_value()
         add_value(tx_amount, get_last_blockchain_value())
     elif user_choice == '2':
         print_blockchain_element()
-    
+    elif user_choice == 'q':
+        break
+    else:
+        print('Input was invalid. Please pick a value from the list.')
+
+
 print('done')
-
-
